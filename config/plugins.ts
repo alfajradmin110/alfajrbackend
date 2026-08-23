971ea0f89856b4214f5ec8304245c8575e14e70f
Amjad Iqbal Khan <amjadiqbalkhanniazi@gmail.com>
2026-08-23 19:03:14 +0500

    again change the name to jwt_secret

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,7 @@
 export default ({ env }: { env: (key: string) => string }) => ({
   'users-permissions': {
     config: {
-      jwtSecret: env('STRAPI_JWT'),
+      jwtSecret: env('JWT_SECRET'),
       jwtManagement: 'refresh',
       sessions: {
         httpOnly: true,
