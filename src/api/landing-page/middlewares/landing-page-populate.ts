b111e4516f195cddc95da3cdb9dffeedbab5ce91
Amjad Iqbal Khan <amjadiqbalkhanniazi@gmail.com>
2026-08-24 23:10:58 +0500

    Again added the query to remove typo:

--- src/api/landing-page/middlewares/landing-page-populate.ts
+++ src/api/landing-page/middlewares/landing-page-populate.ts
@@ -2,46 +2,53 @@
 
 export default (config: any, { strapi }: { strapi: any }) => {
   const myPopulate = {
-  Programs: {
-    on: {
-      "blocks.programs": {
-        populate: {
-          programs: {
-            populate: {
-              images: { fields: ["url", "alternativeText", "name", "width", "height"] },
-              Banner: {
-                populate: {
-                  backgroundImage: { fields: ["url", "alternativeText", "name", "width", "height"] },
+    Programs: {
+      on: {
+        "blocks.programs": {
+          populate: {
+            programs: {
+              populate: {
+                images: {
+                  fields: ["url", "alternativeText", "name", "width", "height"],
+                },
+                Banner: {
+                  populate: {
+                    backgroundImage: {
+                      fields: ["url", "alternativeText", "name", "width", "height"],
+                    },
+                  },
                 },
               },
             },
           },
-          Swiper: {   // <-- must be INSIDE blocks.programs's populate, sibling of "programs"
-            populate: {
-              backgroundImage: { fields: ["url", "alternativeText", "name", "width", "height"] },
+        },
+        "blocks.swiper": {
+          populate: {
+            Swiper: {
+              populate: {
+                backgroundImage: {
+                      fields: ["url", "alternativeText", "name", "width", "height"],
+                    },
+              },
             },
           },
         },
       },
     },
-  },
-};
+  };
 
   return async (ctx: any, next: () => Promise<void>) => {
     ctx.query = ctx.query || {};
-
     ctx.query.populate = myPopulate;
 
     if (ctx.query.status == null) {
       ctx.query.status = "published";
     }
-
     if (ctx.query.locale == null) {
       ctx.query.locale = ["en"];
     }
 
     strapi.log.info("In landing-page-populate middleware.");
-
     await next();
   };
 };
