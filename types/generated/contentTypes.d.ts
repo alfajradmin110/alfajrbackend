2e540cd899d62ee164ee521b324a048a0c3c5ad3
alfajradmin110 <alfajradmin@alfajr.edu.pk>
2026-09-06 00:20:43 +0500

    Added boolean for carousel image grid

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -523,6 +523,8 @@ export interface ApiHighlightHighlight extends Struct.CollectionTypeSchema {
     description: Schema.Attribute.Blocks;
     frontImage: Schema.Attribute.Media<'images'>;
     imageGallery: Schema.Attribute.Media<'images', true>;
+    is_with_banner: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
