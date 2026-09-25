ae2406c9d264c4f091b2f404f6515cb4e7b3b24d
alfajradmin110 <alfajradmin@alfajr.edu.pk>
2026-09-25 07:49:55 +0500

    Remove the error in plugin.ts file from backend

--- config/plugins.ts
+++ config/plugins.ts
@@ -25,18 +25,5 @@ export default ({ env }: { env: (key: string) => string }) => ({
     },
   },
   // ...
-  'tiptap-editor': {
-    config: {
-      presets: {
-        standard: {
-          bold: true,
-          italic: true,
-          // Highlight-start
-          table: true, // Enables the main Table creation utility
-          // Highlight-end
-          image: true,
-        },
-      },
-    },
-  },
+  
 });
